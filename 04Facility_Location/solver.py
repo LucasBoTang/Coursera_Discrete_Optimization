856fdc725356f7eb756b76aae44ed79c089a337f
Apocrypse <lucastang1994@gmail.com>
2018-07-15 10:44:32 -0400

    04Facility_Location

--- 04Facility_Location/solver.py
+++ 04Facility_Location/solver.py
@@ -1,15 +1,19 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
+
 from collections import namedtuple
 import numpy as np
 from matplotlib import pyplot as plt
 import cvxopt as op
+import random
+
 
 
 Point = namedtuple("Point", ['x', 'y'])
 Facility = namedtuple("Facility", ['index', 'setup_cost', 'capacity', 'location'])
 Customer = namedtuple("Customer", ['index', 'demand', 'location'])
+
 
 
 def solve_it(input_data):
@@ -60,7 +64,26 @@ def solve_it(input_data):
     else:
         # start with greedy algorithm
         print('Starting with greedy algorithm...')
-        obj, solution = greedy(facilities, customers, distances)
+        # avoid expensive facilities
+        ban_list = np.argsort([facility.setup_cost for facility in facilities])[::-1][:facility_count//10]
+        initial_obj, initial_solution = greedy(facilities, customers, distances, ban_list)
+        # use tabu search
+        print('Using tabu search...')
+        obj, solution = tabu(facilities, customers, distances, initial_obj, initial_solution)
+        for i in range(4):
+            # intensify
+            if i % 2 == 0:
+                print('Restarting with best solution...')
+                obj, solution = tabu(facilities, customers, distances, obj, solution)
+            # diverisify
+            else:
+                print('Restarting with random solution...')
+                facilities_population = list(range(facility_count))
+                ban_list = random.sample(facilities_population, facility_count//3)
+                rand_obj, rand_solution = greedy(facilities, customers, distances, ban_list)
+                cur_obj, cur_solution = tabu(facilities, customers, distances, rand_obj, rand_solution)
+                if cur_obj < obj:
+                    obj, solution = cur_obj, cur_solution
 
     # visualize
     visualize(facilities, customers, solution)
@@ -70,6 +93,7 @@ def solve_it(input_data):
     output_data += ' '.join(map(str, solution))
 
     return output_data
+
 
 
 def distanceMatirx(facilities_location, customers_location):
@@ -84,6 +108,7 @@ def distanceMatirx(facilities_location, customers_location):
                                   np.square(cur_customer[:, 1] - facilities_location[:, 1]))
 
     return distances
+
 
 
 def branch_bound(facilities, customers, distances, A_ub, b_ub, A_eq, b_eq, c):
@@ -127,31 +152,6 @@ def branch_bound(facilities, customers, distances, A_ub, b_ub, A_eq, b_eq, c):
     branch_bound(facilities, customers, distances, A_ub, b_ub, new_A_eq, new_b_eq, c)
 
 
-def greedy(facilities, customers, distances):
-
-    # initialize
-    solution = [-1] * customer_count
-    capacity_remaining = [facility.capacity for facility in facilities]
-
-    # avoid expensive facilities
-    ban_list = np.argsort([facility.setup_cost for facility in facilities])[::-1][:facility_count//10]
-
-    # assign customer to nearest avaliable facility
-    for c in range(customer_count):
-        sorted_facilities = np.argsort(distances[:, c])
-
-        i = 0
-        f = sorted_facilities[i]
-        while f in ban_list or customers[c].demand > capacity_remaining[f]:
-            i += 1
-            f = sorted_facilities[i]
-
-        capacity_remaining[f] -= customers[c].demand
-        solution[c] = f
-
-    obj = objCal(facilities, customers, distances, solution)
-
-    return obj, solution
 
 
 def objCal(facilities, customers, distances, solution):
@@ -168,6 +168,7 @@ def objCal(facilities, customers, distances, solution):
         obj += distances[f, c]
 
     return obj
+
 
 
 def tableauConstruction(facilities, customers, distances):
@@ -214,8 +215,195 @@ def tableauConstruction(facilities, customers, distances):
     return A_ub, b_ub, A_eq, b_eq, c
 
 
+
+def greedy(facilities, customers, distances, ban_list):
+
+    # initialize
+    solution = [-1] * customer_count
+    capacity_remaining = [facility.capacity for facility in facilities]
+
+    # assign customer to nearest avaliable facility
+    for c in range(customer_count):
+        sorted_facilities = np.argsort(distances[:, c])
+
+        i = 0
+        f = sorted_facilities[i]
+        # check the capacity
+        while f in ban_list or customers[c].demand > capacity_remaining[f]:
+            i += 1
+            # when no feasible solution
+            if i == facility_count:
+                return float('inf'), solution
+            f = sorted_facilities[i]
+
+        # update capacity
+        capacity_remaining[f] -= customers[c].demand
+        solution[c] = f
+
+    # get obj
+    obj = objCal(facilities, customers, distances, solution)
+
+    return obj, solution
+
+
+
+def tabu(facilities, customers, distances, obj, solution):
+
+    # initialize tabu search
+    M = 100 # number of iterations
+    N = 80 # size of neighborhood
+    L = 60 # length of tabu list
+    min_obj, min_solution = obj, solution
+    cur_obj, cur_solution = obj, solution
+
+    # initialize tabu ban_list
+    tabu_list = [set(solution)] * L
+
+    # tabu search
+    obj_list = [cur_obj]
+    find = False
+    for i in range(M):
+
+        neighbors = getNeighbors(cur_solution, facilities, customers, distances, N)
+        # choose a neighbor with tabu
+        n = 0
+        while set(neighbors[n][1]) in tabu_list:
+            # aspiration criteria
+            if min_obj - neighbors[n][0] > 1:
+                break
+            n += 1
+            # when all neighbors are in tabu_list
+            if n == L:
+                break
+
+        if n == L:
+            tabu_list = tabuUpdate(tabu_list, [])
+        else:
+            cur_obj, cur_solution = neighbors[n][0], neighbors[n][1]
+            tabu_list = tabuUpdate(tabu_list, cur_solution)
+            # if find a better solution than initial
+            if cur_obj < min_obj:
+                min_obj, min_solution = cur_obj, cur_solution
+                find = True
+        obj_list.append(cur_obj)
+
+    # visualize when find a better solution
+    if find:
+        plt.ion()
+        plt.xlabel("Iteration")
+        plt.ylabel("Cost")
+        plt.plot(range(M+1), obj_list)
+        plt.pause(5)
+        plt.close()
+
+    return min_obj, min_solution
+
+
+
+def getNeighbors(solution, facilities, customers, distances, N):
+
+    # sperate facilities as used and unused
+    used = set(solution)
+    unused = set(range(facility_count)) - used
+
+    # calculate the number of add. delete and swap
+    add_num = min(int(N * 0.1), len(unused))
+    delete_num = min(int(N * 0.4), len(used))
+    swap_num_1 = int(N * 0.3) # swap used and unused
+    swap_num_2 = N - add_num - delete_num - swap_num_1 # swap both used
+
+    # initialize neighborhood
+    neighbors = []
+
+    # get neighborhood by adding facility
+    add_list = random.sample(unused, add_num)
+    for add in add_list:
+        ban_list = unused - set([add])
+        cur_obj, cur_solution = greedy(facilities, customers, distances, ban_list)
+        neighbors.append([cur_obj, cur_solution])
+
+    # get neighborhood by deleing facility
+    delete_list = random.sample(used, delete_num)
+    for delete in delete_list:
+        ban_list = unused | set([delete])
+        cur_obj, cur_solution = greedy(facilities, customers, distances, ban_list)
+        neighbors.append([cur_obj, cur_solution])
+
+    # get neighborhood by swapping used and unused facility
+    i = 0
+    swap_hash = []
+    while i < swap_num_1:
+        swap = (random.choice(list(used)), random.choice(list(unused)))
+        # avoid selecting same swap
+        if swap in swap_hash:
+            continue
+        else:
+            swap_hash.append(swap)
+        # calculate the demand
+        demand = 0
+        for c in range(customer_count):
+            if solution[c] == swap[0]:
+                demand += customers[c].demand
+        # check feasibility
+        if demand > facilities[swap[1]].capacity:
+            continue
+        # swap facilities
+        cur_solution = [swap[1] if f == swap[0] else f for f in solution]
+        cur_obj = objCal(facilities, customers, distances, cur_solution)
+        neighbors.append([cur_obj, cur_solution])
+        i += 1
+
+    # get neighborhood by swapping used facility
+    i = 0
+    swap_hash = []
+    while i < swap_num_2:
+        swap = random.sample(used, 2)
+        # avoid selecting same swap
+        if swap in swap_hash:
+            continue
+        else:
+            swap_hash.append(swap)
+        # calculate the demand
+        demand_0, demand_1 = 0, 0
+        for c in range(customer_count):
+            f = solution[c]
+            if f == swap[0]:
+                demand_1 += customers[c].demand
+            if f == swap[1]:
+                demand_0 += customers[c].demand
+        # check feasibility
+        if demand_1 > facilities[swap[1]].capacity or demand_0 > facilities[swap[0]].capacity:
+            continue
+        # swap facilities
+        cur_solution = [-1] * customer_count
+        for c in range(customer_count):
+            f = solution[c]
+            if f == swap[0]:
+                cur_solution[c] = swap[1]
+            elif f == swap[1]:
+                cur_solution[c] = swap[0]
+            else:
+                cur_solution[c] = f
+        cur_obj = objCal(facilities, customers, distances, cur_solution)
+        neighbors.append([cur_obj, cur_solution])
+        i += 1
+
+    # sort by obj
+    neighbors.sort(key=lambda neighbor: neighbor[0])
+
+    return neighbors
+
+
+
+def tabuUpdate(tabu_list, cur_solution):
+
+    return tabu_list[1:] +[set(cur_solution)]
+
+
+
 def visualize(facilities, customers, solution):
 
+    plt.ion()
     plt.figure(figsize=(12, 8))
 
     # use different colors
@@ -245,7 +433,9 @@ def visualize(facilities, customers, solution):
         # plot
         plt.scatter(x, y, c=color_list[i%7], marker='p', s=20)
 
-    plt.show()
+    plt.pause(15)
+    plt.close()
+
 
 
 import sys
